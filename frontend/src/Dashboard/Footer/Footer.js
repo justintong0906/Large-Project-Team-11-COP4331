e9b6d858565a288cd799486d7b3d2c5a468c59f1
EclipsedStar <83985361+EclipsedStar0@users.noreply.github.com>
2025-11-05 14:19:58 -0500

    Footer Adjustments
    - Adjusted opacity of footer back to 100% so the text is visible
    - Changed the footer background colour to be #BBBBBB (a silverish colour) (it's a seperate 'class' and makes it so there's a black bar of #111111 above the silver background))
    - Moves footer to the right by 12.85% so it's not hidden by the side panel
    - Made the Attribution header a bit bigger for easier visibility

--- frontend/src/Dashboard/Footer/Footer.js
+++ frontend/src/Dashboard/Footer/Footer.js
@@ -4,10 +4,12 @@ function Footer() {
     
     return(
         <div className="Footer">
-			<h6>Attribution</h6>
-			<a href="https://www.flaticon.com/free-icons/calendar" title="calendar icons">Calendar icons created by Freepik - Flaticon</a>
-			<br />
-			<a href="https://www.flaticon.com/free-icons/exercise" title="exercise icons">Exercise icons created by spacepixel - Flaticon</a>
+			<div className="background_colour_grey">
+				<h4>Attribution</h4>
+				<a href="https://www.flaticon.com/free-icons/calendar" title="calendar icons">Calendar icons created by Freepik - Flaticon</a>
+				<br />
+				<a href="https://www.flaticon.com/free-icons/exercise" title="exercise icons">Exercise icons created by spacepixel - Flaticon</a>
+			</div>
 		</div>
     )
 }
