97150f6b5b008e1931ff6cf6a909e4fad107b1f2
Seth Jones <sethjonesintern@gmail.com>
2025-11-05 13:40:26 -0500

    fixed a couple bugs in user and mailer almost works, just need domain

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
 import { verifyMailerConnection } from "./services/mailer.js";
+import userRoutes from "./routes/userRoutes.js";
+
+
 
 
 dotenv.config();
@@ -16,6 +19,8 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/users", userRoutes);
 
 connectDB();
 verifyMailerConnection();
