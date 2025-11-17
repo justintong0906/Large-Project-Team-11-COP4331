b88f0e720adbc6e57b7837b31a4558463c0ed18b
justintong0906 <justintong0906@gmail.com>
2025-11-17 15:58:22 -0500

    fsdfds

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -5,7 +5,20 @@ import LoginLogo from "./LoginLogo";
 import LoginGraphic from "./LoginGraphic";
 import LoginTitle from "./LoginTitle";
 
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+
 function Login() {
+    const navigate = useNavigate();
+    
+    //send user to dashboard if logged in
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            navigate("/");
+        }
+    }, [navigate]);
+    
     return(
         <div class="Login" style={{display: "flex"}}>
             <LoginLogo/>
